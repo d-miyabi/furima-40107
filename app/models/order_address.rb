0cbf9a0e30dbff1959d6a33313149b16c494d028
d-miyabi <msgh3110+works@gmail.com>
2024-02-18 18:15:07 +0900

    モデルの修正、バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,16 +1,18 @@
-class Order_address
+class OrderAddress
   include ActiveModel::Model
-  attr_accessor :item, :user_id, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :order
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
 
   with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
     validates :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    order = Order.create(item: item, user_id: user_id)
-    address = Address .create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name )
+    order = Order.create(item_id: item_id, user_id: user_id)
+    address = Address .create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name )
   end
 end
