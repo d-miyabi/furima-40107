28a7058a7f9bf9fc7702a1e9a941d5d9d457e841
d-miyabi <msgh3110+works@gmail.com>
2024-02-18 15:14:42 +0900

    コントローラーの作成、アクションの定義

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
-class Purchase_address
+class Order_address
   include ActiveModel::Model
-  attr_accessor :item, :user_id, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :purchase
+  attr_accessor :item, :user_id, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :order
 
   with_options presence: true do
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
@@ -10,6 +10,7 @@ class Purchase_address
   validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    purchase = Purchase.create(item: item, user_id: user_id)
+    order = Order.create(item: item, user_id: user_id)
     address = Address .create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name )
+  end
 end
