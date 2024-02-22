100a2251e517cfb26059fc56d44afb7df728dbc6
d-miyabi <msgh3110+works@gmail.com>
2024-02-22 19:13:57 +0900

    コントローラーに公開鍵を代入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :set_public_key, only: [:index, :create]
   
   def index
     @order_address = OrderAddress.new
@@ -30,5 +31,9 @@ class OrdersController < ApplicationController
         card: order_params[:token],
         currency: 'jpy'
       )
+  end
+
+  def set_public_key
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
   end
 end
