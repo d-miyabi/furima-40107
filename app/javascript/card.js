cb7b1a6e70be5f7fa6fc6ed84f6740d673608c11
d-miyabi <msgh3110+works@gmail.com>
2024-02-22 19:16:39 +0900

    JavaScriptで公開鍵を読み込む記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_37bdf90d0f92b62829661467')
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey)
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
