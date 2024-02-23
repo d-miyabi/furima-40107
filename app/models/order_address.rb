d94a41774b948882ad47493a6768e2e0246851df
d-miyabi <msgh3110+works@gmail.com>
2024-02-23 14:11:50 +0900

    モデルにバリデーションの追加の記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,6 +5,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :user_id, presence: true
+    validates :item_id, presence: true
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
