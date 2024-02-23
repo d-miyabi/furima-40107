93ddad50a44d8d3c7093dd5b3bce64c3e388fe64
d-miyabi <msgh3110+works@gmail.com>
2024-02-23 09:07:27 +0900

    自身が出品した売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移する記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :select_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :redirect_to_show, only: [:edit, :update, :destroy]
+  before_action :redirect_if_sold_out, only:[:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -64,5 +65,11 @@ class ItemsController < ApplicationController
 
   def redirect_to_show
     return redirect_to root_path if current_user.id != @item.user.id
+  end
+
+  def redirect_if_sold_out
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 end
