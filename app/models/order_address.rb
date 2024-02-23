9949d59d3b8898122ed3343fcc969a38979d2ea9
d-miyabi <msgh3110+works@gmail.com>
2024-02-23 14:26:27 +0900

    モデルの重複するpresence: trueの記述をリファクタリング

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,14 +4,14 @@ class OrderAddress
                 :building_name, :phone_number, :token
 
   with_options presence: true do
-    validates :user_id, presence: true
-    validates :item_id, presence: true
+    validates :user_id
+    validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
-    validates :addresses, presence: true
+    validates :addresses
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Do not Include hyphen(-)' }
-    validates :token, presence: true
+    validates :token
   end
 
   def save
