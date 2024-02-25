5cf5fafbb6997defa4a2c734b449d14bda8797ba
d-miyabi <msgh3110+works@gmail.com>
2024-02-25 11:51:26 +0900

    不要なマイグレーションファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_23_120343) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_19_122709) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,7 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_23_120343) do
     t.bigint "order_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "phone_number", null: false
     t.string "prefecture"
     t.index ["order_id"], name: "index_addresses_on_order_id"
   end
