fbd2dd2d32e7b43388bdae4ef6b94ff242df441d
d-miyabi <msgh3110+works@gmail.com>
2024-02-25 17:21:02 +0900

    テストコードの修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -51,17 +51,17 @@ RSpec.describe Order, type: :model do
     it 'phone_numberが9桁以下だと保存できないこと' do
       @order_address.phone_number = '123456789'
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Phone number is invalid. Do not Include hyphen(-)')
+      expect(@order_address.errors.full_messages).to include('Phone number is invalid')
     end
     it 'phone_numberが12桁以上だと保存できないこと' do
       @order_address.phone_number = '1234567890123'
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Phone number is invalid. Do not Include hyphen(-)')
+      expect(@order_address.errors.full_messages).to include('Phone number is invalid')
     end
     it 'phone_numberが英数字以外が含まれている場合は保存できないこと' do
       @order_address.phone_number = 'テスト'
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Phone number is invalid. Do not Include hyphen(-)')
+      expect(@order_address.errors.full_messages).to include('Phone number is invalid')
     end
     it 'tokenが空では登録できないこと' do
       @order_address.token = nil
